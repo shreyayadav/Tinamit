14b47d43d7535189f49f43b5c7e23fc25ec739a6
Shreya <shreya.yadav@mail.mcgill.ca>
2018-09-25 16:58:55 -0400

    First work in DSSAT

--- tinamit/EnvolturasBF/DSSAT2/envoltura.py
+++ tinamit/EnvolturasBF/DSSAT2/envoltura.py
@@ -1,24 +1,4 @@
-from tinamit.BF import ModeloBF
+from tinamit.BF import ModeloDeterminado
 from tinamit.EnvolturasBF.DSSAT2.variable import d_vars
-class Envoltura(ModeloBF):
-    def _cambiar_vals_modelo_externo(símismo, valores):
-        pass
-
-    def _incrementar(símismo, paso, guardar_cada=None):
-        pass
-
-    def _leer_vals(símismo):
-        pass
-
-    def cerrar_modelo(símismo):
-        pass
-
-    def unidad_tiempo(símismo):
-        return 'day'
-
-    def _inic_dic_vars(símismo):
-        pass
-
-    def _leer_vals_inic(símismo):
-        pass
-
+class Envoltura(ModeloDeterminado):
+    
